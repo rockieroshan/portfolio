3d7441f93d2e6f03632b854713f97f1121ababb3
niranjan-sharma-s <niranjansharma1992@gmail.com>
2022-06-07 22:18:50 +0530

    feat added contact component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import gmail from "../images/gmail.jpeg";
+import linkedin from "../images/linkedin.png";
+import github from "../images/github.png";
+import './Contact.css'
 
 const Contact = () => {
   return (
-    <div>Contact Component</div>
-  )
-}
+    <section className="contact">
+      <h1>Contact me</h1>
+      <div className="links">
+        <a href="mailto:niranjansharma1992@gmail.com">
+          <img src={gmail} alt="gmail" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/niranjan-sharma-subramanyam-b7851270"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={linkedin} alt="LinkedIn" />
+        </a>
+        <a
+          href="https://github.com/niranjan-sharma-s"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={github} alt="GiHub" />
+        </a>
+      </div>
+    </section>
+  );
+};
 
-export default Contact
+export default Contact;
